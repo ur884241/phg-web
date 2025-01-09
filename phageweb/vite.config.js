abe86d4163bf25d0e3c01a804e7eba6bb9a43af5
ur884241 <jordi1@outlook.com.br>
2025-01-09 23:36:31 +0000

    Fix deployment configuration

--- phageweb/vite.config.js
+++ phageweb/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.mp4'],
+  build: {
+    outDir: 'dist',
+    assetsInclude: ['**/*.mp4']
+  },
+  server: {
+    port: 3000
+  }
 })
